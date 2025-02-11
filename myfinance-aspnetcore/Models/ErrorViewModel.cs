185c8a3621aa4ae4e951a18ecdac33beceef2450
Lucas Campanelli <lucas.campanelli@outlook.com>
2025-02-11 00:08:12 -0300

    Gerando o novo projeto ASP.NET Core

---
+++ myfinance-aspnetcore/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace myfinance_aspnetcore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
