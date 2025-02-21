eee11e60707c59d169a40c8b855f0772b7ae5bd2
Lucas Campanelli <lucas.campanelli@outlook.com>
2025-02-21 01:35:51 -0300

    Criação do context do banco de dados e correções nas referências dos projetos da solução

--- 4-myfinance-aspnetcore-infra/MyFinanceDbContext.cs
+++ 4-myfinance-aspnetcore-infra/MyFinanceDbContext.cs
@@ -1,6 +1,23 @@
-﻿namespace _4_myfinance_aspnetcore_infra;
+﻿using Microsoft.EntityFrameworkCore;
+using myfinance_aspnetcore_domain.Entities;
 
-public class MyFinanceDbContext
+
+
+namespace myfinance_aspnetcore_infra;
+
+public class MyFinanceDbContext : DbContext
 {
+
+    // Atributo para mapear a tabela PlanoConta
+    public DbSet<PlanoConta> PlanoConta { get; set; }
+    // Atributo para mapear a tabela Transacao
+    public DbSet<Transacao> Transacao { get; set; }
+
+
+    // Método sobrescrito para configurar o banco de dados
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=myfinance.db");
+    }
 
 }
