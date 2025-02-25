aadf709bbf4d32b83c2e81c346fd74cf6a9c4be2
Lucas Campanelli <lucas.campanelli@outlook.com>
2025-02-24 23:37:37 -0300

    Criação da classe de serviços de "Plano de Conta"

--- 2-myfinance-aspnetcore-service/PlanoContaService.cs
+++ 2-myfinance-aspnetcore-service/PlanoContaService.cs
@@ -18,23 +18,62 @@ public class PlanoContaService : IPlanoContaService
         this.contextoBD = contextoBD;
     }
 
+    /// <summary>
+    ///     Método para cadastrar um plano de conta
+    /// </summary>
+    /// <param name="entidade"></param>
     public void Cadastrar(PlanoConta entidade)
     {
-        throw new NotImplementedException();
+        // Obtendo o mapeamento da entidade PlanoConta
+        var dbSet = this.contextoBD.PlanoConta;
+
+        // Verificando se a entidade recebida é nova
+        if (entidade.Id == null)
+        {
+            // Adicionando a entidade ao contexto do banco de dados
+            dbSet.Add(entidade);
+        }
+        else
+        {
+            // Atualizando a entidade no contexto do banco de dados
+            dbSet.Update(entidade);
+        }
+
+        this.contextoBD.SaveChanges();
     }
 
+
+    /// <summary>
+    ///     Método responsavel por excluir um plano de conta
+    /// </summary>
+    /// <param name="id"></param>
+    /// <exception cref="NotImplementedException"></exception> 
     public void Excluir(int id)
     {
-        throw new NotImplementedException();
+        PlanoConta planoContaParaExcluir = new PlanoConta { Id = id };
+
+        this.contextoBD.PlanoConta.Remove(planoContaParaExcluir);
+        
+        this.contextoBD.SaveChanges();
     }
 
+
+    /// <summary>
+    ///    Método responsável por listar todos os planos de conta
+    /// </summary>
+    /// <returns></returns>
     public List<PlanoConta> ListarRegistros()
     {
-        throw new NotImplementedException();
+        return this.contextoBD.PlanoConta.ToList();
     }
 
+
+    /// <summary>
+    ///    Método responsável por retornar um plano de conta pelo id
+    /// </summary>
+    /// <param name="id"></param>
     public PlanoConta RetornarRegistro(int id)
     {
-        throw new NotImplementedException();
+        return this.contextoBD.PlanoConta.Where(x => x.Id == id).First();
     }
 }
