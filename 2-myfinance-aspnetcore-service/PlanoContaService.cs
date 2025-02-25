9a0c0b30dd334068e3a0734849442649c073b5b4
Lucas Campanelli <lucas.campanelli@outlook.com>
2025-02-24 23:06:47 -0300

    Implementando a injeção de dependências para o contexto do banco de dados (Entity Framework)

--- 2-myfinance-aspnetcore-service/PlanoContaService.cs
+++ 2-myfinance-aspnetcore-service/PlanoContaService.cs
@@ -1,5 +1,6 @@
 using myfinance_aspnetcore_service.Interfaces;
 using myfinance_aspnetcore_domain.Entities;
+using myfinance_aspnetcore_infra;
 
 
 
@@ -7,6 +8,16 @@ namespace myfinance_aspnetcore_service;
 
 public class PlanoContaService : IPlanoContaService
 {
+
+    // Contexto do banco de dados do Entity Framework
+    private readonly MyFinanceDbContext contextoBD;
+
+    // Construtor com a injenção de dependência do contexto do banco de dados
+    public PlanoContaService(MyFinanceDbContext contextoBD)
+    {
+        this.contextoBD = contextoBD;
+    }
+
     public void Cadastrar(PlanoConta entidade)
     {
         throw new NotImplementedException();
