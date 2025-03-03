a3e10a7b541846b69c5406539424b9fbbbfe7829
Lucas Campanelli <lucas.campanelli@outlook.com>
2025-03-03 04:38:19 -0300

    Criação da migration para provisão inicial do banco de dados

---
+++ 4-myfinance-aspnetcore-infra/Migrations/20250303065745_ProvisionamentoInicial.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _4_myfinance_aspnetcore_infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProvisionamentoInicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlanoConta",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: false),
+                    Tipo = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlanoConta", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transacao",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Historico = table.Column<string>(type: "TEXT", nullable: false),
+                    Data = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Valor = table.Column<decimal>(type: "TEXT", nullable: false),
+                    PlanoContaId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transacao", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transacao_PlanoConta_PlanoContaId",
+                        column: x => x.PlanoContaId,
+                        principalTable: "PlanoConta",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacao_PlanoContaId",
+                table: "Transacao",
+                column: "PlanoContaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transacao");
+
+            migrationBuilder.DropTable(
+                name: "PlanoConta");
+        }
+    }
+}
