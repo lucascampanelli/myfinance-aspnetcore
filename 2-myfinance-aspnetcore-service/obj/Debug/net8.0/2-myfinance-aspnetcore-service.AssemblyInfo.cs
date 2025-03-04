4d7f15c9d0c0954b4645dbf0653f70986f4b78f8
Lucas Campanelli <lucas.campanelli@outlook.com>
2025-03-04 03:32:41 -0300

    Cadastro, Atualização e Exclusão do Plano de Conta

--- 2-myfinance-aspnetcore-service/obj/Debug/net8.0/2-myfinance-aspnetcore-service.AssemblyInfo.cs
+++ 2-myfinance-aspnetcore-service/obj/Debug/net8.0/2-myfinance-aspnetcore-service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2-myfinance-aspnetcore-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+254ba15cf883accc5c3a0b8026a689f273e51c36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e10a7b541846b69c5406539424b9fbbbfe7829")]
 [assembly: System.Reflection.AssemblyProductAttribute("2-myfinance-aspnetcore-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2-myfinance-aspnetcore-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- myfinance-aspnetcore/Controllers/PlanoContaController.cs
+++ myfinance-aspnetcore/Controllers/PlanoContaController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using myfinance_aspnetcore.Models;
+using myfinance_aspnetcore_domain.Entities;
 using myfinance_aspnetcore_service.Interfaces;
 
 
@@ -48,6 +49,60 @@ namespace myfinance_aspnetcore.Controllers
             ViewBag.ListaPlanoContas = listaPlanoContasModel;
             
             return View();
+        }
+
+        [HttpGet]
+        [Route("Cadastrar")]
+        [Route("Cadastrar/{id}")]
+        public IActionResult Cadastrar(int? id)
+        {
+            // Se o Id for nulo, então é um novo registro
+            if (id == null)
+                return View();
+
+
+            var planoConta = _planoContaService.RetornarRegistro((int)id);
+
+            var planoContaModel = new PlanoContaModel
+            {
+                Id = planoConta.Id,
+                Descricao = planoConta.Descricao,
+                Tipo = planoConta.Tipo
+            };
+
+            return View(planoContaModel);
+        }
+
+        [HttpPost]
+        [Route("Cadastrar")]
+        [Route("Cadastrar/{id}")]
+        public IActionResult Cadastrar(PlanoContaModel model)
+        {
+
+            // Mapeando o PlanoContaModel para a entidade de PlanoConta
+            PlanoConta planoConta = new PlanoConta
+            {
+                Id = model.Id,
+                Descricao = model.Descricao,
+                Tipo = model.Tipo
+            };
+
+            // Se o Id for igual a zero, então é um novo registro.
+            // Caso contrário, atualiza o registro
+            _planoContaService.Cadastrar(planoConta);
+
+            // Redireciona para a rota "Index"
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpGet]
+        [Route("Excluir/{id}")]
+        public IActionResult Excluir(int id)
+        {
+            _planoContaService.Excluir(id);
+
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
